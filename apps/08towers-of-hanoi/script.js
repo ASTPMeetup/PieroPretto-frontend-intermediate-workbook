8fcc318634cc2cd48c70442204f3d91985884764
PieroAPretto <pretto417@gmail.com>
2016-03-31 11:53:01 -0700

    all tests passed & restart function

--- apps/08towers-of-hanoi/script.js
+++ apps/08towers-of-hanoi/script.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function() {
   var block = null;
+  var turns = 0;
   $('[data-stack]').on('click', function(){
     $("#announce-game-won").empty();
     if (block === null) {
@@ -13,6 +14,8 @@ $(document).ready(function() {
       if (blockValue < moveToValue || !moveToValue){
         block.detach();
         $(this).append(block);
+        turns++;
+        console.log(turns);
       }
       else {
         $("#announce-game-won").text('Invalid move');
@@ -26,7 +29,16 @@ $(document).ready(function() {
     var stack2 = $('[data-stack="2"]').children().length;
     var stack3 = $('[data-stack="3"]').children().length;
     if (stack2 === 4 || stack3 === 4) {
-      $("#announce-game-won").text('You won!');
+      $("#announce-game-won").text('You won in '+turns+' turns!');
+
+      //restart game
+      var $stacks = $('[data-stack]');
+      $stacks.find('[data-block]').sort(function(a,b) {
+        //arrange by size
+        return +b.getAttribute('data-block') - +a.getAttribute('data-block');
+      })
+      .appendTo('[data-stack="1"]');
+      turns = 0;
     }
   }
 });
