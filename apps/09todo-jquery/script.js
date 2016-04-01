a18cb5a277263efe7882bf81d941b2de04e6c2f4
PieroAPretto <pretto417@gmail.com>
2016-03-31 22:30:50 -0700

    All specs including bonus

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,5 +1,22 @@
 'use strict';
 
 $(document).ready(function() {
-    // App logic goes here
+	$('#todo-list').sortable();
+	$('li').prepend('<input type="checkbox"/>');
+	$('li').append('<button class="removeitem">remove</button>');
+	$('button').on('click', function(){
+		$(this).parent().remove();
+	});
+
+	$('form').submit(function(event){
+    	event.preventDefault(); 
+		var todoText = $(this).find('#todo').val();
+		$('#todo-list').append('<li id="newtodo">'+todoText+'</li>');
+		$('#newtodo').prepend('<input type="checkbox"/>');
+		$('#newtodo').append('<button class="removeitem">remove</button>');
+		$('li').removeAttr('id');
+		$('button').on('click', function(){
+			$(this).parent().remove();
+		});
+	});
 });
