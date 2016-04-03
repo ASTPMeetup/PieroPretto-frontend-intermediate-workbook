f998bd928876a4b2bd4467579c72b29cc03db720
PieroAPretto <pretto417@gmail.com>
2016-04-03 16:42:24 -0700

    Drier version

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -2,20 +2,17 @@
 
 $(document).ready(function() {
 	$('#todo-list').sortable();
-	$('li').prepend('<input type="checkbox"/>');
-	$('li').append('<button class="removeitem">remove</button>');
-	$('button').on('click', function(){
+	$('li').prepend('<input type="checkbox"/>').append('<button class="removeitem">remove</button>');
+	$(':button').on('click', function(){
 		$(this).parent().remove();
 	});
 
 	$('form').submit(function(event){
     	event.preventDefault(); 
 		var todoText = $(this).find('#todo').val();
-		$('#todo-list').append('<li id="newtodo">'+todoText+'</li>');
-		$('#newtodo').prepend('<input type="checkbox"/>');
-		$('#newtodo').append('<button class="removeitem">remove</button>');
-		$('li').removeAttr('id');
-		$('button').on('click', function(){
+		$('#todo-list').append('<li>'+todoText+'</li>');
+		$('li:last').prepend('<input type="checkbox"/>').append('<button class="removeitem">remove</button>');
+		$(':button').on('click', function(){
 			$(this).parent().remove();
 		});
 	});
