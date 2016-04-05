54995fcc776b9a48a1eac8028e4984e0310078f4
PieroAPretto <pretto417@gmail.com>
2016-04-04 19:43:11 -0700

    Very dry version

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -2,20 +2,24 @@
 
 $(document).ready(function() {
 	$('#todo-list').sortable();
-	$('li').prepend('<input type="checkbox"/>').append('<button>remove</button>');
+	addBoxandButton();
 	deleteItem();
 
 	$('form').submit(function(event){
     	event.preventDefault(); 
 		var todoText = $(this).find('#todo').val();
 		$('#todo-list').append('<li>'+todoText+'</li>');
-		$('li:last').prepend('<input type="checkbox"/>').append('<button>remove</button>');
+		addBoxandButton();
 		deleteItem();
 	});
-	
+
 	function deleteItem() {
 		$(':button').on('click', function(){
 			$(this).parent().remove();
 		});
+	}
+
+	function addBoxandButton() {
+		$('li:last').prepend('<input type="checkbox"/>').append('<button>remove</button>');
 	}
 });
