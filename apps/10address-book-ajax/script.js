2c5bf386a7937699706e63e569c4e3873ea102df
PieroAPretto <pretto417@gmail.com>
2016-04-11 20:20:36 -0700

    easier to read

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -1,32 +1,45 @@
 'use strict';
 
 $(document).ready(function() {
-	$.ajax('https://reqres-api.herokuapp.com/api/users', {
-		success: function(response) {
-			$('tbody').empty();
-			 _.each(response, function(user) {
-			 	var str = '<tr>' + '<td>'+user.id+'</td>' + 
-					 	  '<td>'+user.first_name+'</td>' +
-					 	  '<td>'+user.last_name+'</td>' + 
-			 		 	  '<td><a href="#" data-id="'+user.id+'">view</a></td>' + '</tr>';
-			 	$('tbody').append(str);
-			});
-	 	
-		 	$('[data-id]').on('click', function() {
-				var data_id = $(this).data('id');
-				var url = 'https://reqres-api.herokuapp.com/api/users/';
-				var urlPlusID = url + data_id;
-				$.ajax(urlPlusID, {
-					success: function(user) {
-						var str = '<h3>'+user.first_name+ ' ' +user.last_name+'</h3>' +
-	    						  '<h4>'+user.occupation+'</h4>' +
-	    						  '<p>'+user.phone+'</p>' +
-	    						  '<p>'+user.address+'</p>' +
-	    						  '<img src="'+user.avatar+'">'
-	    				$('#details').html(str);
-	    			}
-	    		});
-			});
-	 	}
-	});
+	populateUsers();
 });
+
+	function populateUsers() {
+		$.ajax('https://reqres-api.herokuapp.com/api/users', {
+			success: function(response) {
+				$('tbody').empty();
+				 _.each(response, function(user) {
+				 	var idCell = '<td>'+user.id+'</td>';
+				 	var firstNameCell = '<td>'+user.first_name+'</td>';
+				 	var lastNameCell = '<td>'+user.last_name+'</td>';
+				 	var urlCell = '<td><a href="#" data-id="'+user.id+'">view</a></td>';
+
+				 	var str = '<tr>' + idCell + firstNameCell + lastNameCell + urlCell + '</tr>';
+				 	$('tbody').append(str);
+				});
+
+				viewUserDetails();
+			}
+		});
+	}
+
+	function viewUserDetails() {
+
+		$('a').on('click', function() {
+			var data_id = $(this).data('id');
+			var url = 'https://reqres-api.herokuapp.com/api/users/';
+			var urlPlusID = url + data_id;
+
+			$.ajax(urlPlusID, {
+				success: function(user) {
+					var nameCell = '<h3>'+user.first_name+ ' ' +user.last_name+'</h3>';
+					var occupationCell = '<h4>'+user.occupation+'</h4>';
+					var phoneNumberCell = '<p>'+user.phone+'</p>';
+					var addressCell = '<p>'+user.address+'</p>';
+					var avatarCell = '<img src="'+user.avatar+'">';
+
+    				$('#details').html(nameCell + occupationCell + phoneNumberCell + addressCell + avatarCell);
+    			}
+	    	});
+		});
+	}
