9c96ee0e9af7229688ae1c27c17bfd542bfa4c94
PieroAPretto <pretto417@gmail.com>
2016-04-13 21:02:00 -0700

    All specs passed with Bootstrap

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -1,5 +1,54 @@
 'use strict';
 
 $(document).ready(function() {
-    // You code here
+	$.ajax('http://localhost:8080/api/gists.json', {
+		success: function(gists) {
+
+			 	var filterPosts = _.filter(gists, function(gist) {
+			 		var postSubStr = gist.description.slice(0,5);
+			 		return postSubStr == "#post";
+			 	});
+
+			 	_.each(filterPosts, function(filterItem) {
+			 		var URL = filterItem.url;
+			 		var description = filterItem.description.slice(5).trim();
+			 		var filterList = '<li><a href="#" data-url="'+URL+'">'+description+'</a></li>';
+			 		$('#posts').prepend(filterList);
+				});
+
+			grabBlogAndComments();
+		}
+	});
+
+    function grabBlogAndComments() {
+		$('a').on('click', function(e){
+			e.preventDefault();
+			var dataUrl = $(this).data('url');
+			$.ajax(dataUrl, {
+				success: function(gist) {
+					var mdContent = gist.files["post.md"].content;
+					var convertedToHTML = marked(mdContent);
+					$('#post').html(convertedToHTML);
+
+					grabComments(gist);
+				}
+			});
+		});
+    }
+
+    function grabComments(gist) {
+		$.ajax(gist.comments_url, {
+			success: function(comments) {
+
+				//delete comments from any previous post
+				$('#comments').empty();
+				
+				_.each(comments, function(comment) {
+					var userInfo = '<b>'+comment.user.login+ ':  ' + '</b>';
+					var bodyInfo = '<i>' + ' -"'+comment.body+'"'+ '</i>';
+					$('#comments').prepend('<p>' + userInfo + bodyInfo + '</p>');
+				});
+			}
+    	});
+	}
 });
